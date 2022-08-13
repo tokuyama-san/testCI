94d1e72e7bd1753a324190ab0fd8dc2cb858385f
Atsuo Tokuyama <tkymato@gmail.com>
2022-08-13 13:05:49 +0900

    vvv

--- app/src/main/java/com/example/myapplication/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/ui/login/LoginActivity.java
@@ -73,7 +73,7 @@ public class LoginActivity extends AppCompatActivity {
                 setResult(Activity.RESULT_OK);
 
                 //Complete and destroy login activity once successful
-                finish();
+//                finish();
             }
         });
 
